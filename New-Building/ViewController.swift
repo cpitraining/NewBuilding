3a023bc06f24f02d45d739c7a993bedd9ca9d953
Smith Frazier <fsmith@smiths-mbp.carolina.rr.com>
2017-07-23 12:50:08 -0400

    Initial Commit

---
+++ New-Building/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  New-Building
+//
+//  Created by Smith, Frazier on 7/23/17.
+//  Copyright © 2017 CPI Security Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
